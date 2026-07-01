07c5e30cbe070363609da306427476241752f8f1
kaushalsandhor <kaushalsandhor17@gmail.com>
2026-07-01 15:08:35 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/kaushal/rentmanagement/RentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.kaushal.rentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaushal/rentmanagement/RentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaushal.rentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
